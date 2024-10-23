effde14e8a775edd38f00aa2a89c8d207546cc49
Artiom Karimov <lunatic0259@gmail.com>
2024-10-23 21:46:47 +0300

    Change readme, add some comments

--- publisher/src/messages/amqp-publish.service.ts
+++ publisher/src/messages/amqp-publish.service.ts
@@ -21,7 +21,7 @@ export class AMQPPublishService implements OnModuleInit {
     this.logger.log('Connecting to rabbit');
     this.connection = amqp.connect(config.amqp.connection, {});
 
-    // Must be bound, othrwise you won't know anything
+    // Must be bound, otherwise you won't know anything
     this.connection.addListener('connectFailed', (data) =>
       this.handleConnectionError(data),
     );
@@ -36,12 +36,16 @@ export class AMQPPublishService implements OnModuleInit {
 
   public async publish({ routingKey, content }: Message): Promise<void> {
     try {
+      /* Note that amqp-connection-manager's publish is not the same as amqplib's publish.
+      This method returns an actual Promise that resolves when broker confirms the message.
+      It rejects if message cannot be sent or is refused by broker.
+      */
       await this.channel.publish(this.exchange, routingKey, content, {
         persistent: true, // Will survive broker restart
       });
     } catch (error) {
       this.logger.error('Cannot publish a message', error);
-      throw error; // This is necessary because transaction success may depend on publishing
+      throw error; // This is necessary because your transaction success may depend on publishing
     }
   }
 
@@ -62,9 +66,10 @@ export class AMQPPublishService implements OnModuleInit {
         durable: true, // Will survive after broker restart
         autoDelete: false, // Will not be destroyed if 0 queues are bound
       });
+      this.logger.log('AMQP Exchange asserted');
     } catch (error) {
       // May throw an error if exchange exists, but has different type
-      this.logger.error('Cannot create exchange', error);
+      this.logger.error('Cannot create AMQP exchange', error);
     }
   }
 }
