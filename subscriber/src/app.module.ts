f455e96b03574150cb164881d4ac096c5e6e85a1
Artiom Karimov <lunatic0259@gmail.com>
2024-10-31 11:55:59 +0300

    Remove unused files & packages

--- subscriber/src/app.module.ts
+++ subscriber/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerModule } from './logger';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [LoggerModule, MessagesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
